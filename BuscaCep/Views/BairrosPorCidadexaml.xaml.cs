07bff5a1eab6e89fe0a679fd7d535ac9fc09ac84
Maria <Usuario@CPD03-M07>
2024-05-20 12:29:13 -0300

    ALteracoes

---
+++ BuscaCep/Views/BairrosPorCidadexaml.xaml.cs
@@ -0,0 +1,19 @@
+namespace BuscaCep.Views;
+
+public partial class BairrosPorCidadexaml : ContentPage
+{
+	public BairrosPorCidadexaml()
+	{
+		InitializeComponent();
+	}
+
+    private void pck_estado_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void pck_cidade_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+}
